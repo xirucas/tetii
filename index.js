cbfb903a7d8f237b19b189a1794c0f296729cccd
Silva <francisilva@deloitte.pt>
2022-12-20 10:46:17 +0000

    teti0.2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const url = "mongodb://SweetDreams:Deloitte303@ac-ojuroaf-shard-00-00.2mzcji7.mongodb.net:27017,ac-ojuroaf-shard-00-01.2mzcji7.mongodb.net:27017,ac-ojuroaf-shard-00-02.2mzcji7.mongodb.net:27017/?ssl=true&replicaSet=atlas-femeg7-shard-0&authSource=admin&retryWrites=true&w=majority"
 const dbName = "app_menu"
 const connect = mongoose.connect(url, { dbName: dbName, useNewUrlParser: true, useUnifiedTopology: true })
-const port = 4000;
+const port = process.env.PORT || 8080;
 app.use(express.json());
 const users = require("./Models/users")
 
